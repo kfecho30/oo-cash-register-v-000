b09089dddd3ee850d6bfff50969a1a380e66dd63
Kelsey Fecho <kfecho30@gmail.com>
2018-03-14 16:29:52 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -33,6 +33,7 @@ class CashRegister
       "There is no discount to apply."
     else
       @total = (1-@discount) * self.total
+      "After the discount, the total comes to #{total}"
     end
   end
 
