49fa99c34c5bff48beaaf702ca42248167913003
Kelsey Fecho <kfecho30@gmail.com>
2018-03-14 16:36:17 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -28,7 +28,7 @@ class CashRegister
   end
 
   def apply_discount
-    if @discount = 0
+    if @discount == 0
       "There is no discount to apply."
     else
       @total = (1-@discount.to_f) *  @total.to_f
